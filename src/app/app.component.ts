40b87aabc729fc7be7cbf991c23a4d54b17005b3
Dylan Morrow <G00365708@gmit.ie>
2021-02-24 15:17:02 +0000

    Lab Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-directive',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-first-app';
+
+  Name: string = "Dylan";
+  Age: number = 22;
+
+
 }
